ef89c9430e41a23705f6bdc7d70f474bb4a93c00
tncbw <tncbw@mst.edu>
2019-11-12 13:22:35 -0600

    Added [LOW,MEDIUM,HIGH] and delete the columns have variance less than 0.009

--- finalR.R
+++ finalR.R
@@ -1,6 +1,6 @@
+y<-read.csv(file="finalFromLIT101.csv")
 
 final<-y
-
 final$LIT101<-cleaningLIT101(final$LIT101)
 final$AIT202<-cleaningAIT202(y$AIT202)
 final$AIT203<-cleaningAIT203(y$AIT203)
@@ -23,3 +23,23 @@ final$PIT502<-cleaningPIT502(y$PIT502)
 final$PIT503<-cleaningAIT502(y$PIT503)
 final$FIT601<-cleaningFIT301(y$FIT601)
 final$AIT201<-cleaningAIT201(y$AIT201)
+final<-applyMethodToAllColumn(final)
+
+'Delete Columns because variance of these collumn were close to 0 which their data are the same to 
+entire of datase:
+[1] "AIT202"
+[1] 0.110298
+[1] "FIT401"
+[1] 0.002506818
+[1] "FIT501"
+[1] 0.0616117
+[1] "FIT502"
+[1] 0.1178485
+[1] "FIT503"
+[1] 0.0006782029
+[1] "FIT504"
+[1] 0.0001105101
+[1] "FIT601"
+[1] 0.009305946
+NOTE: The numeric value is the variance value'
+write.csv(final,file = "~/CS5402/finalFromLIT101Update1.csv")
