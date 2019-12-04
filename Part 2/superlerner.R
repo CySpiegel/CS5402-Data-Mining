e431efedae30493052ea94e19c5076359f526fa2
Matthew Stroble <chimiara999@msn.com>
2019-12-03 22:31:41 -0600

    superlearnercode

--- Part 2/superlerner.R
+++ Part 2/superlerner.R
@@ -1,7 +1,54 @@
 library(SuperLearner)
+library(KernelKnn)
+library(kernlab)
+library(dplyr)
+library(caret)
+library(e1071)
+library(bnlearn)
+library(arules)
+library(arm)
+library(xgboost)
 
+# listWrappers()
 #load data into frame
 data = hw8_data
 
+options(mc.cores = 4)
+getOption("mc.cores")
+set.seed(1, "L'Ecuyer-CMRG")
+
 # Reduce to a dataset random observations.
-train_obs = sample(nrow(data), floor(0.66*nrow(data)))
+trainingSet = sample(seq_len(nrow(data)), size = floor(0.66*nrow(data)))
+xTrain = data[trainingSet,2:38]
+xTest = data[-trainingSet,2:38]
+
+yTrain = as.numeric(data[trainingSet, 1])
+yTest = as.numeric(data[-trainingSet, 1])
+
+SL.kernelKnnManhattan = function(...) {
+  SL.kernelKnn(..., method = "manhattan", k=5)
+}
+
+
+algorithmList = list("SL.mean","SL.ranger","SL.ksvm","SL.kernelKnnManhattan","SL.bayesglm","SL.xgboost")
+
+# algorithmList = list("SL.kernelKnnManhattan")
+#
+system.time({
+crossFoldValidation = CV.SuperLearner(yTrain, xTrain, V = 3, parallel = "multicore", family = binomial(), SL.library = algorithmList)
+})
+
+
+model = SuperLearner(Y = yTrain, X = xTrain, family = binomial(), SL.library = algorithmList)
+prediction = predict.SuperLearner(model, newdata = xTest)
+predictedResult = as.numeric(ifelse(prediction$pred>=0.5,1,0))
+confMatrix = confusionMatrix(as.factor(yTest), as.factor(predictedResult))
+
+system.time({
+cv_sl = CV.SuperLearner(Y = yTrain, X = xTrain, family = binomial(),
+                        # For a real analysis we would use V = 10.
+                        V = 10,
+                        parallel = "multicore",
+                        method = "method.AUC",
+                        SL.library = algorithmList)
+})
